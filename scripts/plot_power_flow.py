2a7a0fef6b81b27c9f088341774a218e56ef8318
Zoltán Marić <5569474+zoltanmaric@users.noreply.github.com>
2022-08-11 11:45:56 +0200

    Jupyter plot helpers for power, loads

--- scripts/plot_power_flow.py
+++ scripts/plot_power_flow.py
@@ -1,5 +1,62 @@
+import pandas as pd
 import plotly.graph_objects as go
 import pypsa
+
+
+def get_marginal_prices(n: pypsa.Network, snapshot, name: str) -> float:
+    marginal_price = n.buses_t.marginal_price.loc[snapshot][name]
+    if -650 < marginal_price < 0:
+        return marginal_price
+    else:
+        return None
+
+
+def get_curtailed_power(n: pypsa.Network, snapshot, name: str) -> float:
+    display(name)
+    # generators_at_bus = n.generators.query(f'bus == "{name}"')
+    generators_at_bus = n.generators.query(f'bus == "7421"')
+    display(generators_at_bus)
+    generators_t_p = n.generators_t.p.filter(generators_at_bus.index).loc[snapshot]
+    print("generators_t_p")
+    display(generators_t_p.T)
+    generators_t_p_max_pu = n.generators_t.p_max_pu[generators_at_bus.index].loc[snapshot]
+    print("generators_t_p_max_pu")
+    display(generators_t_p_max_pu.T)
+    print("denominator")
+    display((generators_t_p_max_pu * generators_at_bus.p_nom).T)
+    curtailed_power_series = generators_t_p / (generators_t_p_max_pu * generators_at_bus.p_nom) * 100
+    rounded = curtailed_power_series.round(decimals=2)
+    print("rounded")
+    display(rounded.T)
+    curtailed_power = rounded
+    print("curtailed_power")
+    display(curtailed_power.T)
+    print("curtailed_power.sum")
+    display(curtailed_power.sum())
+    return curtailed_power.sum()
+
+
+def get_technology_power(n: pypsa.Network, snapshot, name: str, technology: str) -> float:
+    # print('{name}: {value}'.format(value=name, name='name'))
+    generators_at_bus = n.generators.query(f'bus == "{name}"')
+    # print("generators_at_bus")
+    # display(generators_at_bus)
+    tech_specific_generators_at_bus = [g for g in generators_at_bus.index.to_list() if technology in g]
+    # print("tech specific generators at bus")
+    # display(tech_specific_generators_at_bus)
+    generators_t_p = n.generators_t.p.filter(tech_specific_generators_at_bus).loc[snapshot]
+    # print("generators_t_p")
+    # display(generators_t_p)
+    # print('{name}:'.format(value=generators_t_p.sum(), name='generators_t_p.sum()'))
+    # display(generators_t_p.sum())
+    return generators_t_p.sum()
+
+
+def get_loads_power(n: pypsa.Network, snapshot, name: str) -> float:
+    loads_t_p = n.loads_t.p_set.loc[snapshot].filter([name])
+    # print('{name}:'.format(value=loads_t_p, name='loads_t_p'))
+    # display(loads_t_p)
+    return loads_t_p.sum()
 
 
 def colored_network_figure(n: pypsa.Network) -> go.Figure:
@@ -26,16 +83,17 @@ def colored_network_figure(n: pypsa.Network) -> go.Figure:
 
     node_x = []
     node_y = []
-    index = 6
+    index = 331*8 + 4
+    # index = 6
     snapshot = n.snapshots[index]
-    marginal_prices = []
+    print('{name}: {value}'.format(value=snapshot, name='snapshot'))
+    bus_colors = []
     for name, bus in n.buses.iterrows():
-        marginal_price = n.buses_t.marginal_price.loc[snapshot][name]
-        if -650 < marginal_price < 0:
-            x, y = bus.x, bus.y
-            node_x.append(x)
-            node_y.append(y)
-            marginal_prices.append(marginal_price)
+        bus_color = get_loads_power(n, snapshot, name)
+        if bus_color:
+            node_x.append(bus.x)
+            node_y.append(bus.y)
+            bus_colors.append(bus_color)
 
     node_trace = go.Scattermapbox(
         lon=node_x, lat=node_y,
@@ -59,8 +117,8 @@ def colored_network_figure(n: pypsa.Network) -> go.Figure:
             )))
 
     # Color nodes
-    node_trace.marker.color = marginal_prices
-    node_trace.text = marginal_prices
+    node_trace.marker.color = bus_colors
+    node_trace.text = bus_colors
 
     # Create Network Graph
     fig = go.Figure(data=[edge_trace, node_trace],
