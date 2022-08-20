581327fbf24d958a2e4703139dcdec7101db4a54
Zoltán Marić <5569474+zoltanmaric@users.noreply.github.com>
2022-08-20 13:26:34 +0200

    Improve layout of figure on page

--- app.py
+++ app.py
@@ -13,33 +13,43 @@ server = app.server
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        df['year'].min(),
-        df['year'].max(),
-        step=None,
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        id='year-slider'
+    dcc.Graph(
+        id='graph-with-slider',
+        style={'height': '90vh'},
+        figure=dict(layout=dict(autosize=True)),
+        config=dict(responsive=True)
+    ),
+    html.Div(
+        dcc.Slider(
+            df['year'].min(),
+            df['year'].max(),
+            step=None,
+            value=df['year'].min(),
+            marks={str(year): str(year) for year in df['year'].unique()},
+            id='year-slider'
+        ),
+        hidden=True
     )
 ])
+
+n = pypsa.Network('networks/elec_s_all_ec_lv1.01_2H.nc')
+fig = ppf.colored_network_figure(n, 'net_power')
+fig.update_layout(
+    mapbox=dict(center=go.layout.mapbox.Center(lat=55, lon=12), zoom=3.3)
+)
 
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
     Input('year-slider', 'value'))
 def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
+    # filtered_df = df[df.year == selected_year]
 
     # fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
     #                  size="pop", color="continent", hover_name="country",
     #                  log_x=True, size_max=55)
     #
     # fig.update_layout(transition_duration=500)
-
-    n = pypsa.Network('../pypsa-eur/results/networks/elec_s_all_ec_lv1.01_2H.nc')
-    fig = ppf.colored_network_figure(n, 'net_power')
-    fig.update_layout(height=1000, mapbox=dict(center=go.layout.mapbox.Center(lat=55, lon=12), zoom=3.3))
 
     return fig
 
